860e870b24cca1905934d30a98c448e3eae67105
paulabonets <202107466@alu.comillas.edu>
2025-05-18 15:10:49 +0200

    Retoques varios de la página principal y log in

--- src/main/java/com/paulabonets/peliculas/seeder/DataSeeder.java
+++ src/main/java/com/paulabonets/peliculas/seeder/DataSeeder.java
@@ -36,22 +36,22 @@ public class DataSeeder implements CommandLineRunner {
 
     private void seedBooks() {
         bookRepository.saveAll(List.of(
-                new Book("1984", "Distopía clásica de Orwell", parseDate("1949-06-08"), "Ciencia Ficción", "https://img.com/1984.jpg", TypeContent.BOOK, "George Orwell", "9780451524935"),
-                new Book("El Principito", "Un clásico de Saint-Exupéry", parseDate("1943-04-06"), "Fábula", "https://img.com/principito.jpg", TypeContent.BOOK, "Antoine de Saint-Exupéry", "9780156012195"),
-                new Book("Cien años de soledad", "Obra maestra de García Márquez", parseDate("1967-06-05"), "Realismo mágico", "https://img.com/100anos.jpg", TypeContent.BOOK, "Gabriel García Márquez", "9780307474728"),
-                new Book("Fahrenheit 451", "Distopía de Bradbury", parseDate("1953-10-19"), "Ciencia Ficción", "https://img.com/fahrenheit.jpg", TypeContent.BOOK, "Ray Bradbury", "9781451673319"),
-                new Book("Don Quijote", "Novela de Cervantes", parseDate("1605-01-16"), "Aventura", "https://img.com/quijote.jpg", TypeContent.BOOK, "Miguel de Cervantes", "9788491050257")
+                new Book("1984", "Distopía clásica de Orwell", parseDate("1949-06-08"), "Ciencia Ficción", "https://www.estudioenescarlata.com/media/img/portadas/_visd_0001JPG01MGM.jpg", TypeContent.BOOK, "George Orwell", "9780451524935"),
+                new Book("El Principito", "Un clásico de Saint-Exupéry", parseDate("1943-04-06"), "Fábula", "https://th.bing.com/th/id/OIP.3J5Q7LzjrIA8dz31Dh64zQHaKT?rs=1&pid=ImgDetMain", TypeContent.BOOK, "Antoine de Saint-Exupéry", "9780156012195"),
+                new Book("Cien años de soledad", "Obra maestra de García Márquez", parseDate("1967-06-05"), "Realismo mágico", "https://th.bing.com/th/id/OIP.qBIPFhlf2JT3pc0OpMEbpAHaLQ?rs=1&pid=ImgDetMain", TypeContent.BOOK, "Gabriel García Márquez", "9780307474728"),
+                new Book("Fahrenheit 451", "Distopía de Bradbury", parseDate("1953-10-19"), "Ciencia Ficción", "https://imagessl1.casadellibro.com/a/l/t0/81/9788499895581.jpg", TypeContent.BOOK, "Ray Bradbury", "9781451673319"),
+                new Book("Don Quijote", "Novela de Cervantes", parseDate("1605-01-16"), "Aventura", "https://i.pinimg.com/originals/b5/0f/9c/b50f9c7c4969883cfa1dfe5a80763529.jpg", TypeContent.BOOK, "Miguel de Cervantes", "9788491050257")
         ));
     }
 
 
     private void seedMovies() {
         movieRepository.saveAll(List.of(
-                new Movie("Inception", "Sueños dentro de sueños", parseDate("2010-07-16"), "Ciencia Ficción", "https://img.com/inception.jpg", TypeContent.MOVIE, "Christofer Nolan", 148),
-                new Movie("Matrix", "Realidad simulada", parseDate("1999-03-31"), "Ciencia Ficción", "https://img.com/matrix.jpg", TypeContent.MOVIE, "",136),
-                new Movie("Interstellar", "Viajes interestelares", parseDate("2014-11-07"), "Ciencia Ficción", "https://img.com/interstellar.jpg", TypeContent.MOVIE, "", 169),
-                new Movie("Titanic", "Historia de amor en el Titanic", parseDate("1997-12-19"), "Drama/Romance", "https://img.com/titanic.jpg", TypeContent.MOVIE, "", 195),
-                new Movie("El Padrino", "Historia de la mafia", parseDate("1972-03-24"), "Crimen/Drama", "https://img.com/padrino.jpg", TypeContent.MOVIE, "", 175)
+                new Movie("Inception", "Sueños dentro de sueños", parseDate("2010-07-16"), "Ciencia Ficción", "https://cdn.shopify.com/s/files/1/1416/8662/products/inception_2010_imax_original_film_art_2000x.jpg?v=1551890318", TypeContent.MOVIE, "Christofer Nolan", 148),
+                new Movie("Matrix", "Realidad simulada", parseDate("1999-03-31"), "Ciencia Ficción", "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg", TypeContent.MOVIE, "",136),
+                new Movie("Interstellar", "Viajes interestelares", parseDate("2014-11-07"), "Ciencia Ficción", "https://m.media-amazon.com/images/I/91kFYg4fX3L._AC_SL1500_.jpg", TypeContent.MOVIE, "", 169),
+                new Movie("Titanic", "Historia de amor en el Titanic", parseDate("1997-12-19"), "Drama/Romance", "https://c8.alamy.com/comp/PXNB80/titanic-original-movie-poster-PXNB80.jpg", TypeContent.MOVIE, "", 195),
+                new Movie("El Padrino", "Historia de la mafia", parseDate("1972-03-24"), "Crimen/Drama", "https://www.udllibros.com/imagenes/9788418/978841818137.JPG", TypeContent.MOVIE, "", 175)
         ));
     }
 
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Cargando contenido real de MooKFlix...");
+    console.log(" Cargando contenido real de MooKFlix...");
 
     const API_URL = "http://localhost:8080/api/content";
 
@@ -9,19 +9,53 @@ document.addEventListener("DOMContentLoaded", () => {
             if (!response.ok) throw new Error(`Error al obtener ${type}`);
             return await response.json();
         } catch (err) {
-            console.error(`❌ ${type} error:`, err);
+            console.error(` ${type} error:`, err);
             return [];
         }
     }
 
     function renderCards(items, containerId) {
         const container = document.getElementById(containerId);
-        container.innerHTML = ""; // Limpia antes de renderizar
+        container.innerHTML = "";
+
         items.forEach(item => {
-            const div = document.createElement("div");
-            div.className = "highlight-card";
-            div.textContent = item.title || "Sin título";
-            container.appendChild(div);
+            const card = document.createElement("div");
+            card.className = "highlight-card";
+
+            // Parte delantera
+            const front = document.createElement("div");
+            front.className = "card-front";
+
+            const img = document.createElement("img");
+            img.src = item.imageUrl || "img/default.jpg";
+            img.alt = item.title;
+            img.onerror = () => img.style.display = "none";
+
+            const title = document.createElement("div");
+            title.textContent = item.title;
+
+            front.appendChild(img);
+            front.appendChild(title);
+
+            // Parte trasera
+            const back = document.createElement("div");
+            back.className = "card-back";
+
+            back.innerHTML = `
+                <h3>${item.title}</h3>
+                <p><strong>Género:</strong> ${item.genre || "N/A"}</p>
+                <p><strong>Descripción:</strong> ${item.description || "Sin descripción"}</p>
+                <p><strong>Fecha:</strong> ${item.release_date ? new Date(item.release_date).toLocaleDateString() : "N/A"}</p>
+            `;
+
+            card.appendChild(front);
+            card.appendChild(back);
+
+            card.addEventListener("click", () => {
+                card.classList.toggle("flipped");
+            });
+
+            container.appendChild(card);
         });
     }
 
@@ -34,4 +68,23 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     cargarContenido();
+    document.getElementById("btn-movieflix").addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector(".section:nth-of-type(1)").style.display = "block"; // películas
+        document.querySelector(".section:nth-of-type(2)").style.display = "none";  // libros
+    });
+
+    document.getElementById("btn-bookflix").addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector(".section:nth-of-type(1)").style.display = "none";
+        document.querySelector(".section:nth-of-type(2)").style.display = "block";
+    });
+
+    document.getElementById("logo").addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector(".section:nth-of-type(1)").classList.remove("hidden");
+        document.querySelector(".section:nth-of-type(2)").classList.add("hidden");
+
+    });
+
 });
